62e6db1ee1ec0d8513f51a3d85046c6e09a5aaf3
fszhuangb <617670307@qq.com>
2019-04-17 14:47:52 +0800

    add two-sums.cpp

---
+++ two_sums.cpp
@@ -0,0 +1,15 @@
+/* Given an array of integers
+** Return indices of the two numbers such that they add up to a specific target.
+** You may assume that each input would have exactly one solution, 
+** And you may not use the same element twice.
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        
+        
+        
+    }
+};
