51a2def9840c64cc628a998b0f1d54540c47b0b5
fszhuangb <617670307@qq.com>
2019-04-17 15:55:12 +0800

    update

--- two_sums.cpp
+++ two_sums.cpp
@@ -3,13 +3,40 @@
 ** You may assume that each input would have exactly one solution, 
 ** And you may not use the same element twice.
 */
+#include <vector>
+#include <iostream>
 
+/*
+** force method
+**/
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.length(); i++)
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::vector<int> result;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
             {
-                
-            }
+                if (nums[i] + nums[j] == target)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+
+            }       
+        }
+        return result;
     }
 };
+
+
+int main()
+{
+    std::vector<int> num{1, 4, 3, 6, 5};
+    std::vector<int> result;
+    Solution s;
+    result = s.twoSum(num, 7);
+    for (auto i:result)
+        std::cout << i << " ";
+}
