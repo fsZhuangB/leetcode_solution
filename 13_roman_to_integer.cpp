d4b0f537fc18216f34d7f55e6448b63edd9efa8b
fszhuangb <617670307@qq.com>
2019-07-11 23:08:06 +0800

    add new question

--- 13_roman_to_integer.cpp
+++ 13_roman_to_integer.cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <iostream>
 
 using namespace std;
@@ -11,46 +11,31 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> valueMap;
-        valueMap['I'] = 1;
-        valueMap['V'] = 5;
-        valueMap['X'] = 10;
-        valueMap['L'] = 50;
-        valueMap['C'] = 100;
-        valueMap['D'] = 500;
-        valueMap['M'] = 1000;
-        int size = s.length();
-        int arr[size], i, j;
-        int result = 0;
-        for (i = 0; i < size; i++)
-        {
-            arr[i] = valueMap[s[i]];
-            cout << arr[i] << endl;
-        }
-        
-        for (j = 0; j < size; j++)
-        {
-            if (arr[j - 1] < arr[j])
-            {
-                result += arr[j] - arr[j - 1];
-                j++;
-            }
-            else 
-            {
-                result += arr[j - 1];
-                if (j == size - 1)
-                {
-                    result += arr[j];
-                }
-            }
-        }
+       map<char, int> value_map;
+       value_map['I'] = 1;
+       value_map['V'] = 5;
+       value_map['X'] = 10;
+       value_map['L'] = 50;
+       value_map['C'] = 100;
+       value_map['D'] = 500;
+       value_map['M'] = 1000;
+       int result = 0, i, j;
+       result += value_map[s[0]];
+       for (i = 0, j = 1; j < s.size(); ++i, ++j)
+       {
+           result += value_map[s[j]];
+           if (value_map[s[i]] < value_map[s[j]])
+           {
+               result = result - 2 * value_map[s[i]];
+           }
+       }
         return result;
     }
 };
 
 int main()
 {
-    string s = "III";
+    string s = "XL";
     Solution sl;
     int num = sl.romanToInt(s);
     cout << num << endl;
