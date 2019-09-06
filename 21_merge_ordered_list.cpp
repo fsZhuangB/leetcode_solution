1d3f78e8f8c5e5127ca42226c30f0d429259fbc2
fszhuangb <617670307@qq.com>
2019-09-06 12:41:29 +0800

    go

--- 21_merge_ordered_list.cpp
+++ 21_merge_ordered_list.cpp
@@ -142,21 +142,20 @@ class Solution2
             return l1;
         else
         {
-            if (l1->val > l2->val)
-               { 
-                    l2->next = mergeTwoLists(l1, l2->next);
-                    return l2;
-               }
-            else
+            /** 若l1较小，则将l1当作头节点 */
+            if (l1->val < l2->val)
+                {
+                    l1->next = mergeTwoLists(l1->next, l2);
+                    return l1;
+                }
+            else 
             {
-                l1->next = mergeTwoLists(l1->next, l2);
-                return l1;
-            }   
-        }
+                l2->next = mergeTwoLists(l1, l2->next);
+                return l2;
+
+            }
+        }   
     }
-
-
-
 };
 
 int main()
