b45a88c5bc2c57d5d143da648a4605772ddb307a
fszhuangb <617670307@qq.com>
2019-09-17 10:08:13 +0800

    good

--- reverse_link_list.cpp
+++ reverse_link_list.cpp
@@ -20,6 +20,7 @@ public:
         /** 递归方法 */
         if (head == nullptr || head->next == nullptr)
             return head;
+        /** 其实该地址空间一直存放着最后一个节点的地址 */
         ListNode *newHead = reverseList(head->next);
         head->next->next = head;
         head->next = nullptr;
@@ -28,8 +29,27 @@ public:
     }
 };
 
+class Solution2 {
+    public:
+    ListNode* reverseList2(ListNode* head)
+    {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        ListNode *p = head;
+        ListNode *newHead = nullptr;
+        while (p != nullptr)
+        {
+            ListNode *tmp = p->next;
+            p->next = newHead;
+            newHead = p;
+            p = tmp;
+        }
+        return newHead;
+    }
+};
+
 int main()
 {
-    
+
     return 0;
 }
