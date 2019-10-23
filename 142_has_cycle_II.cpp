ceaa740d320cca45be2e5cae5750f9a719700f6c
fszhuangb <617670307@qq.com>
2019-10-23 08:59:20 +0800

    no pass

---
+++ 142_has_cycle_II.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <set>
+
+class ListNode {
+    public:
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        std::set<ListNode *> set_list;
+        while (head != nullptr) {
+            if (set_list.count(head) == 1)
+                return head;
+            else
+                set_list.insert(head);
+            head = head->next;
+        }
+        return nullptr;    
+    }
+};
