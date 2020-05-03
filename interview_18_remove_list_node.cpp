913657ff1d229edfb91afe5766f8682ce34e1ada
fszhuangb <fszhuangb@gmail.com>
2020-05-03 15:42:14 +0800

    modify remove list node

--- interview_18_remove_list_node.cpp
+++ interview_18_remove_list_node.cpp
@@ -18,21 +18,25 @@ public:
     ListNode* deleteNode(ListNode* head, int val) {
         if (head == NULL)
             return NULL;
+        // we have to pay attention to the condition:
+        // [3,4,5] we want to delete 3
         if (head->val == val)
             return head->next;
-        ListNode* willDeleted = head;
-        while (willDeleted->val != val)
-            willDeleted = willDeleted->next;
+
+        // This code is not necessary, so we can depreacted this
+        // ListNode* willDeleted = head;
+        // while (willDeleted->val != val)
+        //     willDeleted = willDeleted->next;
 
         ListNode* pre = head;
         ListNode* cur = head->next;
-        while (cur != willDeleted)
+        while (cur->val != val)
         {
             pre = pre->next;
             cur = cur->next;
         }
-        pre->next = willDeleted->next;
-        delete willDeleted;
+        pre->next = cur->next;
+        delete cur;
         return head;
     }
 };
