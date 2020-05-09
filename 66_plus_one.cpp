0ea15af3e9558aaf295d2b0931247c51ed8694ec
fszhuangb <fszhuangb@gmail.com>
2020-05-09 19:33:49 +0800

    add plus_one

--- 66_plus_one.cpp
+++ 66_plus_one.cpp
@@ -5,7 +5,30 @@
  * and each element in the array contain a single digit.
  * You may assume the integer does not contain any leading zero, except the number 0 itself.
 */
-int main()
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution 
 {
-    
-}
+    public:
+    vector<int> plusOne(vector<int>& digits)
+    {
+        for (int i = digits.size() - 1; i > 0; i--)
+        {
+            ++digits[i];
+            // if digits don't need to carry, we can simply return digits
+            if (digits[i] != 10)
+                return digits;
+            else
+            // or we need to set the bit to 0, and carry
+                digits[i] = 0;
+        }
+        /**
+         * If the first digits need to be (carry) plus one, like 9999, we have to insert 1 into it
+        */
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+
+};
