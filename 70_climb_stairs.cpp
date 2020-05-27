ff01a3ff1652173c5b97ce5f1d77d93941bcc12c
fszhuangb <fszhuangb@gmail.com>
2020-05-27 08:46:28 +0800

    add new solution

--- 70_climb_stairs.cpp
+++ 70_climb_stairs.cpp
@@ -25,3 +25,33 @@ class Solution {
             return dp[n];
         }
 };
+
+/**
+ * Recursion solution:
+ * This solution i use recursion to calculate every simple condition:
+ * n == 1 && n == 2 condition
+*/
+class Solution2 {
+    public:
+    int climbStairs(int n)
+    {
+        return climbStairs(0, n);
+
+    }
+    /**
+     * i指目前阶数
+     * n指目标阶数
+    */
+    int climbStairs(int i, int n)
+    {
+        if (i > n)
+        {
+            return 0;
+        }
+        if (i == n)
+        {
+            return 1;
+        }
+        return climbStairs(i + 1, n) + climbStairs(i + 2, n);
+    }
+};
