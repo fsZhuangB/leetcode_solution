cb2899160c405aaafb5dd364c678aec11c66f5af
fszhuangb <fszhuangb@gmail.com>
2020-05-27 08:35:26 +0800

    add

--- 70_climb_stairs.cpp
+++ 70_climb_stairs.cpp
@@ -9,22 +9,19 @@
 using namespace std;
 
 class Solution {
-public:
-    int climbStairs(int n) {
-        // 初始条件
-        if (n <= 1)
+    public:
+        int climbStairs(int n)
         {
-            return n;
+            if (n <= 1)
+                return n;
+            vector<int> dp(1000, 0);
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; ++i)
+            {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n];
         }
-        vector<int> dp(10000, 0);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; ++i)
-        {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
-        
-    }
 };
